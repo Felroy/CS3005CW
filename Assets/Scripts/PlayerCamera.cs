f402aa031ced5b8ac25948a58e60edd2f102c4b0
Felroy <felroy01998@gmail.com>
2020-11-13 20:01:38 +0000

    added camera control

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform playerLoc;
+    public float camIntensity;
+    Vector3 offset;
+    float minimumY; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - playerLoc.position;
+        minimumY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetCamLoc = playerLoc.position + offset;
+        transform.position = Vector3.Lerp (transform.position, targetCamLoc, camIntensity*Time.deltaTime);
+        
+        if(transform.position.y < minimumY) {
+            transform.position = new Vector3 (transform.position.x, minimumY, transform.position.z);
+        }
+    }
+}
