5b3e554fa48515b24d0953d0ecb97ef04a7d8414
Felroy <felroy01998@gmail.com>
2020-11-27 22:03:54 +0000

    game completed

--- Assets/Scripts/BossHP.cs
+++ Assets/Scripts/BossHP.cs
@@ -7,13 +7,16 @@ public class BossHP : MonoBehaviour
 { 
     float currentBossHP;
     float phase2;
-    public float BossMaxHP = 100;    
+    public float BossMaxHP = 100; 
+    Rigidbody2D bossRB;   
 
     
     //enemy HP HUD variables
     public Slider enemyHPSlider;  
     public Gradient enemyGrad;
     public Image fill;
+    public Text winText;
+    public bool bossDead;
 
 
     //enemy loot
@@ -22,6 +25,7 @@ public class BossHP : MonoBehaviour
 
     void Start()
     {
+        bossRB = GetComponent<Rigidbody2D>();
         currentBossHP = BossMaxHP;
         enemyHPSlider.maxValue = currentBossHP;
         enemyHPSlider.value = currentBossHP;  
@@ -47,12 +51,15 @@ public class BossHP : MonoBehaviour
         }
     }
 
-    void killBoss(){              
-        Destroy(gameObject);
-        //Instantiate(enemyDeathFX, transform.position, transform.rotation);
+    void killBoss(){
+        bossRB.velocity = Vector2.zero;
+        //death animation, create Animator something; something.GetComponent<Animator>(); or just GetComponent<Animator>().SetBool("bossDead", true); ##remember to create bossDead bool on animator 
+        GetComponent<Animator>().SetBool("bossDead", true);    
+        Destroy(gameObject, 1);
+        Animator gameOver = winText.GetComponent<Animator>();
+        gameOver.SetTrigger("win");
         if(loot){
             Instantiate(potion, transform.position, transform.rotation);
-        }
-        
+        }        
    }
 }
